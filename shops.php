3db87923969e940a22e30da665b22416435408c6
kkirrin <khlebnikov-2016@mail.ru>
2024-11-12 13:34:42 +1000

    feat: Добавил шаблоны

--- shops.php
+++ shops.php
@@ -1,85 +1,91 @@
-   <main>
+<?php
+/*
+    Template Name: магазины
+    */
+?>
 
-       <section id="shops">
-           <div class="container relative">
+<main>
+    <section id="shops">
+        <div class="container relative">
 
-               <div>
-                   <ul class="flex gap-[10px]">
-                       <li class=" text-darkGray">
-                           <a href="/">
-                               Главная
-                           </a>
-                       </li>
+            <div>
+                <ul class="flex gap-[10px]">
+                    <li class=" text-darkGray">
+                        <a href="/">
+                            Главная
+                        </a>
+                    </li>
 
-                       <li class=" text-darkGray">
-                           —
-                       </li>
+                    <li class=" text-darkGray">
+                        —
+                    </li>
 
-                       <li class="breadcrumb__item">
-                           <span class=" text-darkGray">Магазины</span>
-                       </li>
-                   </ul>
-               </div>
-
-
-               <div class="py-[10px]">
-                   <h2 class="font-bold md:text-[55px] text-[28px] uppercase">
-                       Магазины
-                   </h2>
-               </div>
-
-               <div class="flex gap-[20px]">
-                   <button data-target-id="0" class="tab_btn active">Списком</button>
-                   <button data-target-id="1" class="tab_btn">На карте</button>
-               </div>
-
-               <div class="w-full flex md:flex-row flex-col gap-[50px] ">
+                    <li class="breadcrumb__item">
+                        <span class=" text-darkGray">Магазины</span>
+                    </li>
+                </ul>
+            </div>
 
 
-                   <div id="content-0" class="tab-content active" data-id="0">
-                       <table class="table">
-                           <thead>
-                               <tr>
-                                   <th>Название</th>
-                                   <th>Адрес</th>
-                                   <th>Телефон</th>
-                                   <th>Режим работы</th>
-                               </tr>
-                           </thead>
-                           <tbody>
-                               <tr>
-                                   <td>ФОРМАТ ВКУСА</td>
-                                   <td>Город, ул. Затупкина 16, второй этаж</td>
-                                   <td>+7 900 000 00 00</td>
-                                   <td>Ежедневно с 9:00 до 18:00</td>
-                               </tr>
-                               <tr>
-                                   <td>ФОРМАТ ВКУСА</td>
-                                   <td>Город, ул. Затупкина 16, второй этаж</td>
-                                   <td>+7 900 000 00 00</td>
-                                   <td>Ежедневно с 9:00 до 18:00</td>
-                               </tr>
-                               <tr>
-                                   <td>ФОРМАТ ВКУСА</td>
-                                   <td>Город, ул. Затупкина 16, второй этаж</td>
-                                   <td>+7 900 000 00 00</td>
-                                   <td>Ежедневно с 9:00 до 18:00</td>
-                               </tr>
-                           </tbody>
-                       </table>
-                   </div>
+            <div class="py-[10px]">
+                <h2 class="font-bold md:text-[55px] text-[28px] uppercase">
+                    Магазины
+                </h2>
+            </div>
 
-                   <div id="content-1" class="tab-content" data-id="1">
-                       <div class="flex flex-col gap-[30px]">
-                           <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aba3db466796bacc28218ba02da75eaf5e1a8e3f2c9c46c200ee9903b7332ad00&amp;width=900&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
-                           <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A804feeb871678cd5ab3973396ee139a8a57ac470ec0734117acd4a80a55477ad&amp;width=900&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
-                           <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa732a35eee20f7335d99cbe595bb9ca8ee30c11b3d5f77a0d0d60320aea1aae1&amp;width=900&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
-                       </div>
-                   </div>
+            <div class="flex gap-[20px]">
+                <button data-target-id="0" class="tab_btn active">Списком</button>
+                <button data-target-id="1" class="tab_btn">На карте</button>
+            </div>
+
+            <div class="w-full flex md:flex-row flex-col gap-[50px] ">
 
 
-               </div>
-           </div>
-       </section>
+                <div id="content-0" class="tab-content active" data-id="0">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Название</th>
+                                <th>Адрес</th>
+                                <th>Телефон</th>
+                                <th>Режим работы</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>ФОРМАТ ВКУСА</td>
+                                <td>Город, ул. Затупкина 16, второй этаж</td>
+                                <td>+7 900 000 00 00</td>
+                                <td>Ежедневно с 9:00 до 18:00</td>
+                            </tr>
+                            <tr>
+                                <td>ФОРМАТ ВКУСА</td>
+                                <td>Город, ул. Затупкина 16, второй этаж</td>
+                                <td>+7 900 000 00 00</td>
+                                <td>Ежедневно с 9:00 до 18:00</td>
+                            </tr>
+                            <tr>
+                                <td>ФОРМАТ ВКУСА</td>
+                                <td>Город, ул. Затупкина 16, второй этаж</td>
+                                <td>+7 900 000 00 00</td>
+                                <td>Ежедневно с 9:00 до 18:00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-   </main>
+                <div id="content-1" class="tab-content" data-id="1">
+                    <div class="flex flex-col gap-[30px]">
+                        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aba3db466796bacc28218ba02da75eaf5e1a8e3f2c9c46c200ee9903b7332ad00&amp;width=900&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+                        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A804feeb871678cd5ab3973396ee139a8a57ac470ec0734117acd4a80a55477ad&amp;width=900&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+                        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa732a35eee20f7335d99cbe595bb9ca8ee30c11b3d5f77a0d0d60320aea1aae1&amp;width=900&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
