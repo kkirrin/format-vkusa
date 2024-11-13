6eca1cc42e848f62d20aded8281639b4b271db39
kkirrin <khlebnikov-2016@mail.ru>
2024-11-13 15:33:56 +1000

    feat: добавил страницы

---
+++ sale.php
@@ -0,0 +1,37 @@
+<?php
+/*
+    Template Name: Товары по скидке и акции
+    */
+?>
+
+<?php get_header(); ?>
+
+<main>
+
+
+    <section>
+        <div class="container relative">
+            <div>
+                <ul class="flex gap-[10px]">
+                    <li class=" text-darkGray">
+                        <a href="/">
+                            Главная
+                        </a>
+                    </li>
+
+                    <li class=" text-darkGray">
+                        —
+                    </li>
+
+                    <li class="breadcrumb__item">
+                        <span class=" text-darkGray">Товары по скидке или по акции</span>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </section>
+
+</main>
+
+<?php get_footer(); ?>
